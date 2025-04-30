7e519bb8dea52ac5153a966007685a0d9d39730b
alavuvi <ville-pekka.alavuotunki@proton.me>
2025-04-30 13:32:08 +0300

    korjauksia

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ const moonIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
 const sunIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>`
 const btn = document.getElementById("theme-toggle")
 
-// Teema-valinta tallennetaan localStorageen
+// Teeman haku localstoragesta
 const savedTheme = localStorage.getItem('theme');
 if (savedTheme) {
     document.body.classList.remove('dark-theme', 'light-theme');
@@ -10,7 +10,7 @@ if (savedTheme) {
     btn.innerHTML = savedTheme === 'dark-theme' ? moonIcon : sunIcon;
 }
 
-// teemavalinta toiminallisuus
+// teemavalinta ja tallennus localstorageen
 btn.addEventListener("click",() => {
     const body = document.body
     if (body.classList.contains('dark-theme')) {
